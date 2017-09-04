f7ec544082798164f321a6ba9b9bd9019e678b83
marat <sadykovmr@tatsotsbank.ru>
2017-09-04 17:35:36 +0300

    hw7 1.1 realized

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -6,7 +6,8 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.*;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class RootControllerTest extends AbstractControllerTest {
@@ -25,5 +26,39 @@ public class RootControllerTest extends AbstractControllerTest {
                                 hasProperty("name", is(USER.getName()))
                         )
                 )));
+    }
+
+    @Test
+    public void testMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItems(
+                        both(
+                                hasProperty("id", is(MEAL1.getId())))
+                                .and(
+                                        hasProperty("description", is(MEAL1.getDescription()))
+                                ),
+                        both(
+                                hasProperty("id", is(MEAL2.getId())))
+                                .and(
+                                        hasProperty("calories", is(MEAL2.getCalories()))
+                                ),
+                        both(
+                                hasProperty("id", is(MEAL3.getId())))
+                                .and(
+                                        hasProperty("calories", is(MEAL4.getCalories()))
+                                )
+                                .and(
+                                        hasProperty("exceed", is(false))),
+
+                        hasProperty("id", is(MEAL4.getId())),
+                        hasProperty("id", is(MEAL5.getId())),
+                        hasProperty("id", is(MEAL6.getId()))
+                )))
+        ;
     }
 }
